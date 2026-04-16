8f7c9c5c24699078199b21167ee072e0ec43ab6e
Artoriaz <42828250+Artoriaz@users.noreply.github.com>
2026-04-16 18:51:03 -0400

    Add: surrounded-regions - submission-12

---
+++ Data Structures & Algorithms/surrounded-regions/submission-12.java
@@ -0,0 +1,39 @@
+class Solution {
+    int[][] directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+    int ROWS, COLS;
+    public void solve(char[][] board) {
+        ROWS = board.length;
+        COLS = board[0].length;
+        for(int c= 0; c < COLS; c++) {
+            if(board[0][c] == 'O') dfs(0, c, board);
+            if(board[ROWS-1][c] == 'O')  dfs(ROWS-1, c, board);
+
+        }
+        for(int r = 0; r < ROWS ; r++) {
+            if(board[r][COLS-1] == 'O') dfs(r, COLS-1, board);
+            if(board[r][0] == 'O')  dfs(r, 0, board);
+                
+        }
+
+        for(int i = 0 ; i < ROWS; i++) {
+            for(int j = 0; j < COLS ; j++) {
+                if(board[i][j] == '#') board[i][j] = 'O';
+                else board[i][j] = 'X';
+            }
+        }
+    }
+
+    public void dfs(int row, int col, char[][] board) {
+        board[row][col] = '#';
+        for(int[] dir: directions) {
+            int nRow = dir[0] + row;
+            int nCol = dir[1] + col;
+            if(isValid(nRow, nCol, board)) {  
+                    dfs(nRow,nCol, board);
+            }
+        }
+    }
+    public boolean isValid(int row, int col, char[][] grid) {
+        return 0 <= row && row < ROWS && 0 <= col && col < COLS && grid[row][col] == 'O';
+    }
+}
