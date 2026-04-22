c69bf62ef93bc376cc40c325cda6d61e943b4ac2
Artoriaz <42828250+Artoriaz@users.noreply.github.com>
2026-04-22 16:12:52 -0400

    Add: redundant-connection - submission-0

---
+++ Data Structures & Algorithms/redundant-connection/submission-0.java
@@ -0,0 +1,38 @@
+class Solution {
+    public class DSU {
+        int[] parent;
+        public DSU(int n) {
+            this.parent = new int[n];
+            for(int i = 1; i < n; i++){
+                parent[i]  = i;
+            }
+        }
+
+        public int find(int node) {
+            int temp = node;
+            while( temp != parent[temp]) {
+                parent[temp] = parent[parent[temp]];
+                temp = parent[temp];
+            }
+            return temp;
+        }
+        public boolean union(int a, int b) {
+            int parentA = find(a);
+            int parentB = find(b);
+            // if they have the same parent they're already connected
+            if(parentA == parentB) return false;
+
+            parent[parentA] = parentB;
+            return true;
+            
+        }
+    }
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        DSU dsu = new DSU(n +1);
+        for(int[] e: edges) {
+            if(!dsu.union(e[0], e[1])) return e;
+        }
+        return new int[0];
+    }
+}
