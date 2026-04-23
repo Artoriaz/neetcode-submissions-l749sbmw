67c94497cd9ebf242e2d188e9f3f1823dea3ad2c
Artoriaz <42828250+Artoriaz@users.noreply.github.com>
2026-04-23 15:00:44 -0400

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.java
@@ -0,0 +1,13 @@
+class Solution {
+    int[] cache;
+    public int climbStairs(int n) {
+        cache = new int[n];
+        Arrays.fill(cache, -1);
+        return dfs(0, n);
+    }
+    public int dfs(int i, int n) {
+        if(i >= n) return i == n ? 1 : 0;
+        if(cache[i] != -1) return cache[i];
+        return cache[i] = dfs(i +1 , n) + dfs(i +2, n);
+    }
+}
