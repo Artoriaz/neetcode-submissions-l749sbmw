5caab18c48d0314671798efb5f59b2a49348fe2d
Artoriaz <42828250+Artoriaz@users.noreply.github.com>
2026-06-29 15:14:21 -0400

    Add: isomorphic-strings - submission-1

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length() != t.length()) return false;
+        Map<Character,Character> mapST = new HashMap<>();
+        Map<Character, Character> mapTS = new HashMap<>();
+        for(int i =0 ; i < s.length() ; i++) {
+            char a = s.charAt(i) , b = t.charAt(i);
+            if(mapST.containsKey(a)) {
+                if(mapST.get(a) != b) return false;
+            } else {
+                mapST.put(a, b);
+            }
+
+            if(mapTS.containsKey(b)) {
+                if(mapTS.get(b) !=a) return false;
+            } else {
+                mapTS.put(b, a);
+            }
+        }
+        return true;
+    }
+    
+}
