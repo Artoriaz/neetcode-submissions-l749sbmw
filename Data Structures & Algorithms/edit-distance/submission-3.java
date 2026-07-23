a7dc6d6a59ba433331964fd8a9bf5dcc91ad8357
Artoriaz <42828250+Artoriaz@users.noreply.github.com>
2026-07-23 15:32:01 -0400

    Add: edit-distance - submission-3

---
+++ Data Structures & Algorithms/edit-distance/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        Integer[][] cache = new Integer[word1.length()][word2.length()];
+
+        return dfs(0,0, word1,word2,cache);
+    }
+    public int dfs(int i , int j , String s1, String s2, Integer[][] cache) {
+        if( i == s1.length()) return s2.length() -j;
+        if( j == s2.length()) return s1.length() -i;
+        if(cache[i][j] != null) return cache[i][j];
+
+        if(s1.charAt(i) == s2.charAt(j)) {
+            cache[i][j] = dfs( i+1, j+1, s1,s2,cache);
+        } else {
+            int res = Math.min(1 + dfs(i+1, j, s1, s2, cache),1+ dfs(i , j+1, s1,s2,cache));
+            res = Math.min(res, 1+ dfs( i+1, j+1, s1,s2,cache));
+            cache[i][j] = res;
+        }
+        
+        return cache[i][j];
+    }
+}
